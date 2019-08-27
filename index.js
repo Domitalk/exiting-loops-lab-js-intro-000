23379490058c2f33a2437f1e883b44585cd1f6bb
Dominic <dominic.m.chu@gmail.com>
2019-08-27 01:22:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function breakOut(array, changeValue, stopValue) {
   for (let i = 0; i < array.length; i++) {
-    if (typeof array[i] !== changeValue) {
+    if (array[i] !== changeValue) {
       array[i] = changeValue;
     }
-    
+    if (array[i] === stopValue) {
+      return array[i];
+    }
   }
 }
