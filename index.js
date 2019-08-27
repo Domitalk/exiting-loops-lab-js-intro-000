0f55f0b858b02605e78b0ae5e0782686330e1a29
Dominic <dominic.m.chu@gmail.com>
2019-08-27 01:31:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,5 +20,9 @@ function keepGoing(array, changeValue, skipValue) {
 }
 
 function findBy(array, findFn) {
-  
+  for (let i = 0; i < array.length; i++) {
+    if (findFn(array[i])) {
+      
+    }
+  }
 }
