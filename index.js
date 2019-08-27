063dc6bfe1022ff5acc1a7aed535720dd9a25434
Dominic <dominic.m.chu@gmail.com>
2019-08-27 01:29:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ function breakOut(array, changeValue, stopValue) {
     }
   }
 }
+
+function keepGoing(array, changeValue, skipValue) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === skipValue) {
+      continue;
+    }
+    array[i] = changeValue;
+  }
+  return array;
+}
